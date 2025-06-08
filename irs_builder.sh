3c4ce8bb58dc3654dc70fa9150ac1ce58dad7fb9
Sophie <sophiaasophieee@gmail.com>
2025-06-08 01:27:09 -0400

    Update irs_builder.sh

--- irs_builder.sh
+++ irs_builder.sh
@@ -120,6 +120,7 @@ log_info "Done. Have fun!"
 
 echo -e "${COLOR_MAGENTA_B}Credits"
 echo -e "${COLOR_PINK_B}Sophia${COLOR_RESET}: The lead developer of IRS, Figured out wifi"
+echo -e "${COLOR_YELLOW_B}Synaptic${COLOR_RESET}: Emotional Support"
 echo -e "${COLOR_CYAN_B}Simon${COLOR_RESET}: Brainstormed how to do wifi, helped with dhcpcd"
 echo -e "${COLOR_BLUE_B}kraeb${COLOR_RESET}: QoL improvements and initial idea"
 echo -e "${COLOR_GREEN_B}xmb9${COLOR_RESET}: The name, helping with builder and init and part of shimbooting"
